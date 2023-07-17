3e9646104fea80e96250816937841601cddf4310
syaruchyk <syaruchyk@gmail.com>
2023-07-17 15:05:01 +0200

    actualizcion 23.07.17

---
+++ Curso de Introducción a la programación/Ejercicios tema 9/Tema_9/src/Persona.java
@@ -0,0 +1,87 @@
+public class Persona {
+    public int edad;
+    public String nombre;
+    public String telefono;
+
+    public Persona(int edad, String nombre, String telefono) {
+        this.edad = edad;
+        this.nombre = nombre;
+        this.telefono = telefono;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+}
+
+class Cliente extends Persona {
+    public int credito;
+
+    public Cliente(int edad, String nombre, String telefono, int credito) {
+        super(edad, nombre, telefono);
+        this.credito = credito;
+    }
+
+    public int getCredito() {
+        return credito;
+    }
+
+    public void setCredito(int credito) {
+        this.credito = credito;
+    }
+}
+
+class Trabajador extends Persona {
+    private double salario;
+
+    public Trabajador(int edad, String nombre, String telefono, double salario) {
+        super(edad, nombre, telefono);
+        this.salario = salario;
+    }
+
+    public double getSalario() {
+        return salario;
+    }
+
+    public void setSalario(double salario) {
+        this.salario = salario;
+    }
+}
+
+class Main {
+    public static void main(String[] args) {
+        Cliente cliente = new Cliente(30, "Juan", "123456789", 1000);
+        System.out.println("Cliente:");
+        System.out.println("Edad: " + cliente.getEdad());
+        System.out.println("Nombre: " + cliente.getNombre());
+        System.out.println("Teléfono: " + cliente.getTelefono());
+        System.out.println("Crédito: " + cliente.getCredito());
+
+        Trabajador trabajador = new Trabajador(35, "María", "987654321", 2000.50);
+        System.out.println("\nTrabajador:");
+        System.out.println("Edad: " + trabajador.getEdad());
+        System.out.println("Nombre: " + trabajador.getNombre());
+        System.out.println("Teléfono: " + trabajador.getTelefono());
+        System.out.println("Salario: " + trabajador.getSalario());
+    }
+}
